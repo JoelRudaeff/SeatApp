75f9673f136f960d3db4eafbbd2298a3af45d06c
JoelRudaeff <joelrudaeff1@gmail.com>
2017-02-05 19:57:05 +0200

    Code Upgrade
    Improved the code. Made the code "smarter".

--- Arduino.cpp
+++ Arduino.cpp
@@ -1,27 +1,47 @@
-[sourcecode language=”cpp”]
 
 /*
 TRIGGER_PIN - Arduino pin tied to trigger pin on the ultrasonic sensor
 ECHO_PIN - Arduino pin tied to echo pin on the ultrasonic sensor
 */
 
+#include <Arduino.h>
 #include <NewPing.h>
 #include <string>
+#include <sstream> 
 
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm
 #define SONAR_NUM 4      // Number of sensors
-#dfine SENSORS_MARK 2   // a number which states that in a string to the rpi, after the x number comes a sensor's result- x0x1x1x1
+#define START_PIN 4     // From which pin does the sequence of sensors start ( input, output )
 
 //Array of sensors (objects)
 NewPing sonar[SONAR_NUM]; // Each sensor's trigger pin, echo pin, and max distance to ping
+
+//previous seats data
+stringstream old_data_to_send;
+
+//which lines does the arduino backups
+string responsible_lines = "1:2";
 
 using namespace std; // just in case
 
 void setup() 
 {
-	for ( int i= 4; i< SONAR_NUM + 4*2 ; i+=2)
-		//currently ports 4,5 : 6,7 : 8,9 : 10,11 
-		sonar[i] = new NewPing(i, i+1, MAX_DISTANCE)
+	int i;// for accessing the array of sensors
+	int j;// for pin numbers
+
+	for (i = 0, j = START_PIN; i < START_PIN * 3; i += 1, j += 2)
+	{
+		try
+		{
+			sonar[i] = new NewPing(j, j + 1, MAX_DISTANCE) //currently ports 4,5 : 6,7 : 8,9 : 10,11 
+		}
+		catch // if for some reason the pins weren't availbe or something like that
+		{
+			i -= 1; //next sensors will be init instead of the current 
+		}
+	}
+		
+		
 		
 	Serial.begin(9600); // The serial is actually the usb, which we want to send the data to. The usb will send it forward to the Raspberry Pi. Open serial monitor at 115200 baud to see ping results
 }
@@ -34,23 +54,26 @@ void loop()
 	
 	for( i = 0; i < SONAR_NUM; i++)
 	{
-		distance = sonar[i].ping_cm();
+		distance = sonar[i].ping_cm(); //returns 0 if nothing found in range
+
 		if ( distance > 0 && distance < 200)
 			distance = "1"; // person was found inrange	
 		else
 			distnace = "0"; // person wasn't found
 		
-		data_to_send.append(SENSORS_MARK + distance);
+		data_to_send.append(distance);
 	}
 	
-	//send data to the rpi
-	serial.println(data_to_send);
+	if (old_data_to_send.str() != data_to_send) //if the new data isn't the same as the old data
+	{
+		//by the protocol
+		serial.println('S' + data_to_send.length() + data_to_send + responsible_lines.length() + responsible_lines); //send data to the rpi
+		old_data_to_send.str(data_to_send);
+	}
+		
 	
-	delay(2000); // Wait 2000ms between pings
+	delay(500); // Wait 0.5s between pings
 }
-
-
-[/sourcecode]
 
 
 /* The Raspberry Pi code (an example of recieving messages from the arduino):
--- RaspberryPi.py
+++ RaspberryPi.py
@@ -1,4 +1,4 @@
-import serial #will be valid inside the rpi
+import serial
 import time
 import socket
 
@@ -6,7 +6,6 @@ PORT = 5839 #TODO: port to send data to server - DEFINE A DEFAULT PORT FOR THE S
 HOST = "127.0.0.1" #TODO: ip of server - DEFINE IP FOR THE SERVER
 PROTOCOL_UPDATE_KEY = "9" #TODO: number of message that the server will understand that the RPI sends updated data - DEFINE A KEY FOR UPDATE THE DATA
 PROTOCOL_CLOSE_KEY = "10" #TODO: message that will be sent to the server if the RPI is closing the code - DEFINE A KEY FOR CLOSING THE COMMUNICATION
-SENSORS_MARK = "2" #TODO: in a string from the arduino, mentions that after the number:x comes a sensors data - DEFINE A NUMBER FOR THE MARK IN BOTH RPI AND ARDO
 
 """ list given with the data: 1 - if a person was detected, 0 - if a person wasn't detected in sensor no.[i]
 every cell in the list belongs to a sensor and its result, for example: [0] - result of first sensor, [1] - result of second sensor and so on"""
@@ -26,15 +25,30 @@ def handle_missions(socket_to_server):
     ser = serial.Serial('/dev/ttyACM0', 9600)
     stored_sensors_data = [-1,-1,-1,-1] #for the first run of the functions, then, it changes to the results themselves
     while True:
-        list_of_sensors = ser.readline().split(SENSORS_MARK) # turn the given string into a list
+        list_of_sensors = list(ser.readline()) # turn the given string into a list of chars
         send_sensors_data_to_server(list_of_sensors.pop(0),stored_sensors_data,socket_to_server) #first cell in the list is always '', so remove it.
         time.sleep(2) #sleep for TWO seconds ( 2s )
 
+
+#will try to connect to the server 
+def try_connecting_to_server(socket_to_server):
+    connected = False
+    while not connected:
+        try:
+            socket_to_server.connect((HOST, PORT))  # connect to the server
+            connected = True
+        except:
+            print "couldn't connect to server, sleeping for 0.5 seconds"
+            time.sleep(0.5)
+
+    return socket_to_server
+	
+	
 def main():
-    # start the socket and connect to server
+    # start the socket to the server
     socket_to_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # setup the socket for future use
     try:
-        socket_to_server.connect((HOST,PORT))
+        socket_to_server = try_connecting_to_server(socket_to_server)
         handle_missions(socket_to_server) # pass the socket
     except:
         print "encountered an error"
