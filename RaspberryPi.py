f210a8d41657e0ae9ffcaa7b6eb1b56386b4b3bf
JoelRudaeff <joelrudaeff1@gmail.com>
2017-02-26 19:56:05 +0200

    RPI update
    Little update to the RPI update

--- RaspberryPi.py
+++ RaspberryPi.py
@@ -8,32 +8,13 @@ PROTOCOL_UPDATE_KEY = "9" #TODO: number of message that the server will understa
 PROTOCOL_CLOSE_KEY = "10" #TODO: message that will be sent to the server if the RPI is closing the code - DEFINE A KEY FOR CLOSING THE COMMUNICATION
 SERIAL_PORT = 9600 # port for communication between the RPI and the Arduino
 
-""" list given with the data: 1 - if a person was detected, 0 - if a person wasn't detected in sensor no.[i]
-every cell in the list belongs to a sensor and its result, for example: [0] - result of first sensor, [1] - result of second sensor and so on"""
-def send_sensors_data_to_server(sensors_data,stored_sensors_data,socket_to_server):
-    sensors_data_in_string = "".join(sensors_data)  # turn the list to a string
-    stored_sensors_data_in_string = "".join(stored_sensors_data) # turn the list to a string
-    if (stored_sensors_data_in_string == "-1-1-1-1") or (stored_sensors_data_in_string != sensors_data_in_string): # if stored data is different than the new data or it's first time, else don't do anything
-        server_replay  = ""
-        socket_to_server.sendall(PROTOCOL_UPDATE_KEY + sensors_data_in_string)
-        socket_to_server.recv(server_replay)
-        print server_replay
-        stored_sensors_data = sensors_data # LIST - is a mutable type - passes as a reference and can be changed, SO if the new sensors data is different than the older data, redirect the to the new list of data
-
-"""function that will do all the work, get data from the arduino (serial) and update data to server"""
-def handle_missions(socket_to_server):
-    #serial - our "physical socket" to the sensors and the arduino - gets the data of the sensors from the output of the arduino code we made
-    ser = serial.Serial('/dev/ttyACM0', SERIAL_PORT)
-    stored_sensors_data = [-1,-1,-1,-1] #for the first run of the functions, then, it changes to the results themselves
-    while True:
-        list_of_sensors = list(ser.readline()) # turn the given string into a list of chars
-        send_sensors_data_to_server(list_of_sensors.pop(0),stored_sensors_data,socket_to_server) #first cell in the list is always '', so remove it.
-        time.sleep(2) #sleep for TWO seconds ( 2s )
-
 
 #will try to connect to the server 
 def try_connecting_to_server(socket_to_server):
+	# start the socket to the server
+    socket_to_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # setup the socket for future use
     connected = False
+	
     while not connected:
         try:
             socket_to_server.connect((HOST, PORT))  # connect to the server
@@ -44,18 +25,36 @@ def try_connecting_to_server(socket_to_server):
 
     return socket_to_server
 	
+""" list given with the data: 1 - if a person was detected, 0 - if a person wasn't detected in sensor no.[i]
+every cell in the list belongs to a sensor and its result, for example: [0] - result of first sensor, [1] - result of second sensor and so on"""
+def send_sensors_data_to_server(sensors_data,stored_sensors_data,socket_to_server):
+    sensors_data_in_string = "".join(sensors_data)  # turn the list to a string
+    stored_sensors_data_in_string = "".join(stored_sensors_data) # turn the list to a string
+    
+	if (stored_sensors_data_in_string == "-1-1-1-1") or (stored_sensors_data_in_string != sensors_data_in_string): # if stored data is different than the new data or it's first time, else don't do anything
+        server_replay  = ""
+        socket_to_server.sendall(PROTOCOL_UPDATE_KEY + sensors_data_in_string)
+        socket_to_server.recv(server_replay)
+        print server_replay
+        stored_sensors_data = sensors_data # LIST - is a mutable type - passes as a reference and can be changed, SO if the new sensors data is different than the older data, redirect the to the new list of data
+	socket_to_server.close()
+	
+"""function that will do all the work, get data from the arduino (serial) and update data to server"""
+def handle_missions():
+    #serial - our "physical socket" to the sensors and the arduino - gets the data of the sensors from the output of the arduino code we made
+    ser = serial.Serial('/dev/ttyACM0', SERIAL_PORT)
+    stored_sensors_data = [-1,-1,-1,-1] #for the first run of the functions, then, it changes to the results themselves
+    while True:
+        list_of_sensors = list(ser.readline()) # turn the given string into a list of chars
+        send_sensors_data_to_server(list_of_sensors.pop(0),stored_sensors_data,try_connecting_to_server()) #first cell in the list is always '', so remove it.
+        time.sleep(1) #sleep for ONE seconds ( 1s )
+
 	
 def main():
-    # start the socket to the server
-    socket_to_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # setup the socket for future use
     try:
-        socket_to_server = try_connecting_to_server(socket_to_server)
-        handle_missions(socket_to_server) # pass the socket
+        handle_missions() 
     except:
         print "encountered an error"
-    finally:
-        socket_to_server.sendall(PROTOCOL_CLOSE_KEY)
-        socket_to_server.close()
 
 if __name__ == "__main__":
     main()
