33c72f08da816dfbfa66ee01e857770e02f21cff
Liran <netaliran23@gmail.com>
2017-03-04 01:24:10 +0200

    Client
    The User Interface which contains 3 screens - the main screen which lead
    to the bus schedule screen or to the train schedule screen, due to the
    choose of the user.

---
+++ Client/SeatApp/app/src/main/java/com/example/user/seatapp/BusScheduleActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.seatapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class BusScheduleActivity extends ActionBarActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_schedule);
+    }
+}
