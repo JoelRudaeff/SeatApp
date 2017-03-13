baa31845be3a1bb99fc06934657d4cf72598e2dd
Liran <netaliran23@gmail.com>
2017-03-13 23:14:34 +0200

    Client

--- Client/SeatApp/app/src/main/java/com/example/user/seatapp/MainActivity.java
+++ Client/SeatApp/app/src/main/java/com/example/user/seatapp/MainActivity.java
@@ -18,133 +18,26 @@ public class MainActivity extends ActionBarActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText Train_Current_place = (EditText) findViewById(R.id.Train_Current_place);
-        final EditText Train_Destination_place = (EditText) findViewById(R.id.Train_Destination_place);
-        final EditText Bus_Current_place = (EditText) findViewById(R.id.Bus_Current_place);
-        final EditText Bus_Destination_place = (EditText) findViewById(R.id.Bus_Destination_place);
-        final EditText Taxi_Current_place = (EditText) findViewById(R.id.Taxi_Current_place);
-        final EditText Taxi_Destination_place = (EditText) findViewById(R.id.Taxi_Destination_place);
-
-        final Button Train_schedule_button = (Button) findViewById(R.id.Train_schedule_button);
-        final Button Bus_schedule_button = (Button) findViewById(R.id.Bus_schedule_button);
-        final Button Taxi_schedule_button = (Button) findViewById(R.id.Taxi_schedule_button);
-
-        final RadioButton Train_Radio_Button = (RadioButton) findViewById(R.id.train_radio_button);
-        final RadioButton Bus_Radio_Button = (RadioButton) findViewById(R.id.bus_radio_button);
-        final RadioButton Taxi_Radio_Button = (RadioButton) findViewById(R.id.Taxi_radio_button);
-
-        Train_Radio_Button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked)
-            {
-                if (isChecked) //when the user chooses the train button, show the train's current place and destination place, and hide the other inputs
-                {
-                    Train_Current_place.setVisibility(buttonView.VISIBLE);
-                    Train_Destination_place.setVisibility(buttonView.VISIBLE);
-                    Train_schedule_button.setVisibility(buttonView.VISIBLE);
-                    Bus_Radio_Button.setChecked(false);
-                    Taxi_Radio_Button.setChecked(false);
-                }
-
-                else
-                {
-                    Train_Current_place.setVisibility(buttonView.INVISIBLE);
-                    Train_Current_place.setText(""); //Erases the current place which the user entered before
-                    Train_Destination_place.setVisibility(buttonView.INVISIBLE);
-                    Train_Destination_place.setText(""); //Erases the destination place which the user entered before
-                    Train_schedule_button.setVisibility(buttonView.INVISIBLE);
-                }
-            }
-        });
-
-        Bus_Radio_Button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-            {
-                if (isChecked) //when the user chooses the bus button, show the bus's current place and destination place, and hide the other inputs
-                {
-                    Bus_Current_place.setVisibility(buttonView.VISIBLE);
-                    Bus_Destination_place.setVisibility(buttonView.VISIBLE);
-                    Bus_schedule_button.setVisibility(buttonView.VISIBLE);
-                    Train_Radio_Button.setChecked(false);
-                    Taxi_Radio_Button.setChecked(false);
-                }
-
-                else
-                {
-                    Bus_Current_place.setVisibility(buttonView.INVISIBLE);
-                    Bus_Current_place.setText(""); //Erases the current place which the user entered before
-                    Bus_Destination_place.setVisibility(buttonView.INVISIBLE);
-                    Bus_Destination_place.setText(""); //Erases the destination place which the user entered before
-                    Bus_schedule_button.setVisibility(buttonView.INVISIBLE);
-                }
-
-            }
-        });
-
-        Taxi_Radio_Button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-            {
-                if (isChecked) //when the user chooses the bus button, show the bus's current place and destination place, and hide the other inputs
-                {
-                    Taxi_Current_place.setVisibility(buttonView.VISIBLE);
-                    Taxi_Destination_place.setVisibility(buttonView.VISIBLE);
-                    Taxi_schedule_button.setVisibility(buttonView.VISIBLE);
-                    Train_Radio_Button.setChecked(false);
-                    Bus_Radio_Button.setChecked(false);
-                }
-
-                else
-                {
-                    Taxi_Current_place.setVisibility(buttonView.INVISIBLE);
-                    Taxi_Current_place.setText(""); //Erases the current place which the user entered before
-                    Taxi_Destination_place.setVisibility(buttonView.INVISIBLE);
-                    Taxi_Destination_place.setText(""); //Erases the destination place which the user entered before
-                    Taxi_schedule_button.setVisibility(buttonView.INVISIBLE);
-                }
-
-            }
-        });
-
+        final Button Train_schedule_button = (Button) findViewById(R.id.Train_button);
+        final Button Bus_schedule_button = (Button) findViewById(R.id.Bus_button);
+        final Button Taxi_schedule_button = (Button) findViewById(R.id.Taxi_button);
     }
 
-    public void startBusSchedule(View view) // when the button is clicked, open this activity
+    public void startBuses(View view) // when the button is clicked, open this activity
     {
-        final EditText Bus_Current_place = (EditText) findViewById(R.id.Bus_Current_place);
-        final EditText Bus_Destination_place = (EditText) findViewById(R.id.Bus_Destination_place);
-
         Intent intent = new Intent(this, BusScheduleActivity.class);
-        intent.putExtra("Current", Bus_Current_place.getText().toString());
-        intent.putExtra("Destination", Bus_Destination_place.getText().toString());
-
         startActivity(intent);
     }
 
-    public void startTrainSchedule(View view) // when the button is clicked, open this activity
+    public void startTrains(View view) // when the button is clicked, open this activity
     {
-        final EditText Train_Current_place = (EditText) findViewById(R.id.Train_Current_place);
-        final EditText Train_Destination_place = (EditText) findViewById(R.id.Train_Destination_place);
-
-        Intent intent = new Intent(this, TrainScheduleActivity.class);
-        intent.putExtra("Current", Train_Current_place.getText().toString());
-        intent.putExtra("Destination", Train_Destination_place.getText().toString());
-
+        Intent intent = new Intent(this, TrainsActivity.class);
         startActivity(intent);
     }
 
-    public void startTaxiSchedule(View view) // when the button is clicked, open this activity
+    public void startTaxis(View view) // when the button is clicked, open this activity
     {
-        final EditText Taxi_Current_place = (EditText) findViewById(R.id.Taxi_Current_place);
-        final EditText Taxi_Destination_place = (EditText) findViewById(R.id.Taxi_Destination_place);
-
         Intent intent = new Intent(this, TaxiScheduleActivity.class);
-        intent.putExtra("Current", Taxi_Current_place.getText().toString());
-        intent.putExtra("Destination", Taxi_Destination_place.getText().toString());
-
         startActivity(intent);
     }
 
---
+++ Client/SeatApp/app/src/main/java/com/example/user/seatapp/TrainsActivity.java
@@ -0,0 +1,63 @@
+package com.example.user.seatapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class TrainsActivity extends ActionBarActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trains);
+
+        Spinner current_spinner = (Spinner) findViewById(R.id.CurrentSpinner);
+        Spinner destination_spinner = (Spinner) findViewById(R.id.DestinationSpinner);
+
+        TextView st = (TextView) findViewById(R.id.stam);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.places_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        current_spinner.setAdapter(adapter);
+        destination_spinner.setAdapter(adapter);
+
+        current_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
+            {
+                Object item = parent.getItemAtPosition(pos);
+                TextView st = (TextView) findViewById(R.id.stam);
+                st.setText(item.toString());
+            }
+
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+            }
+        });
+
+        destination_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
+            {
+                Object item = parent.getItemAtPosition(pos);
+            }
+
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+            }
+        });
+
+        //String current_place = current_spinner.getSelectedItem().toString();
+        //String destination_place = destination_spinner.getSelectedItem().toString();
+    }
+}
