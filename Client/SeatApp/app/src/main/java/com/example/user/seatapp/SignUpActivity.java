48e1cf1c36b5838a3b76032dff36b5103cad9704
Liran <netaliran23@gmail.com>
2017-03-14 20:38:11 +0200

    Client
    Sign-up with conditions to the usernameand checking if the re-password
    is the same as the password.

--- Client/SeatApp/app/src/main/java/com/example/user/seatapp/SignUpActivity.java
+++ Client/SeatApp/app/src/main/java/com/example/user/seatapp/SignUpActivity.java
@@ -1,11 +1,13 @@
 package com.example.user.seatapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends ActionBarActivity
 {
@@ -26,14 +28,48 @@ public class SignUpActivity extends ActionBarActivity
 
     public void startHomePage(View view)
     {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
         final EditText NewUserName = (EditText) findViewById(R.id.SignUpUsername);
         final EditText NewPassword = (EditText) findViewById(R.id.SignUpPassword);
+        final EditText RePassword = (EditText) findViewById(R.id.SignUpPasswordAgain);
 
-        Intent intent = new Intent(this, HomePageActivity.class);
-        intent.putExtra("NewUsername", NewUserName.getText().toString());
-        intent.putExtra("NewPassword", NewPassword.getText().toString());
+        String name = NewUserName.getText().toString();
 
-        startActivity(intent);
+        //Checking if the username was written by the rules
+        if (! (name.matches("^[a-zA-Z0-9]{4,12}+") )) //if the username contains special characters or shorter/longer than the usual
+        {
+            if (name.length() < 4 || name.length() > 12)
+            {
+                Toast name_length = Toast.makeText(context, "The username needs to be between 4 to 12 characters", duration);
+                name_length.show();
+            }
+
+            else
+            {
+                Toast name_special_characters = Toast.makeText(context, "Username can't include special characters!", duration);
+                name_special_characters.show();
+            }
+        }
+
+        else
+        {
+            //Checking the password and it's re-password
+            if (!NewPassword.getText().toString().equals(RePassword.getText().toString())) //if the password and the re-password doesn't equal
+            {
+                Toast toast_passwords = Toast.makeText(context, "Please enter again your password and re-enter it!", duration);
+                toast_passwords.show();
+            }
+
+            else {
+                Intent intent = new Intent(this, HomePageActivity.class);
+                intent.putExtra("NewUsername", NewUserName.getText().toString());
+                intent.putExtra("NewPassword", NewPassword.getText().toString());
+
+                startActivity(intent);
+            }
+        }
     }
 
 }
--- Client/SeatApp/app/src/main/java/com/example/user/seatapp/TrainsActivity.java
+++ Client/SeatApp/app/src/main/java/com/example/user/seatapp/TrainsActivity.java
@@ -1,7 +1,6 @@
 package com.example.user.seatapp;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -20,7 +19,7 @@ public class TrainsActivity extends ActionBarActivity
         Spinner current_spinner = (Spinner) findViewById(R.id.CurrentSpinner);
         Spinner destination_spinner = (Spinner) findViewById(R.id.DestinationSpinner);
 
-        TextView st = (TextView) findViewById(R.id.stam);
+        TextView st = (TextView) findViewById(R.id.cur);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.places_array, android.R.layout.simple_spinner_item);
@@ -36,8 +35,8 @@ public class TrainsActivity extends ActionBarActivity
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
             {
                 Object item = parent.getItemAtPosition(pos);
-                TextView st = (TextView) findViewById(R.id.stam);
-                st.setText(item.toString());
+                TextView cur = (TextView) findViewById(R.id.cur);
+                cur.setText(item.toString());
             }
 
             public void onNothingSelected(AdapterView<?> parent)
@@ -50,6 +49,8 @@ public class TrainsActivity extends ActionBarActivity
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
             {
                 Object item = parent.getItemAtPosition(pos);
+                TextView dest = (TextView) findViewById(R.id.dest);
+                dest.setText(item.toString());
             }
 
             public void onNothingSelected(AdapterView<?> parent)
