bdf8f1dca5f7d8eb8920480337a58a04fdd75be7
liranneta23 <netaliran23@gmail.com>
2017-04-13 17:52:06 +0300

    New changes
    Insert the statuses of each line into the Data Base of a vehicle

---
+++ RPI/RaspberryPi(1).py
@@ -0,0 +1,104 @@
+import serial
+import time
+import socket
+
+PORT = 8886  # TODO: port to send data to server - DEFINE A DEFAULT PORT FOR THE SERVER
+HOST = '10.10.0.14'  # TODO: ip of server - DEFINE IP FOR THE SERVER
+PROTOCOL_UPDATE_KEY = "u"  # TODO: number of message that the server will understand that the RPI sends updated data - DEFINE A KEY FOR UPDATE THE DATA
+PROTOCOL_CLOSE_KEY = "c"  # TODO: message that will be sent to the server if the RPI is closing the code - DEFINE A KEY FOR CLOSING THE COMMUNICATION
+SERIAL_PORT = 9600  # port for communication between the RPI and the Arduino
+AMOUNT_OF_LINES = 10  #how many lines are in the vehicle
+SEATS = {}
+
+#vehicle information - hard coded
+VEHICLE_TYPE = "bus"
+VEHICLE_COMPANY = "egged"
+VEHICLE_NUMBER = "263"
+
+# will try to connect to the server
+def try_connecting_to_server():
+    # start the socket to the server
+    socket_to_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # setup the socket for future use
+    connected = False
+
+    while connected == False:
+        try:
+            socket_to_server.connect((HOST, PORT))  # connect to the server
+            connected = True
+            print "connected to server!"
+        except:
+            print "couldn't connect to server, sleeping for 0.5 seconds"
+            time.sleep(0.5)
+
+    return socket_to_server
+
+
+    
+""" list given with the data: 1 - if a person was detected, 0 - if a person wasn't detected in sensor no.[i]
+every cell in the list belongs to a sensor and its result, for example: [0] - result of first sensor, [1] - result of second sensor and so on"""
+
+
+def send_sensors_data_to_server(line_number,sensors_data, stored_sensors_data, socket_to_server):
+    stored_sensors_data = ""
+
+    if line_number in SEATS: #if seats were already init on that line
+        stored_sensors_data = SEATS[line_number]
+    else:
+        SEATS[line_number] = "-1-1-1-1" #init
+        stored_sensors_data = SEATS[line_number]
+
+    sensors_data_len = len(sensors_data) #what is the number of the seats which were sent to the RPi
+    stored_sensors_data_len = len(stored_sensors_data) #what is the number of the seats which were sent to the RPi
+    sensors_data_in_string = "".join(sensors_data)  # turn the list to a string
+    stored_sensors_data_in_string = "".join(stored_sensors_data)  # turn the list to a string
+
+
+    print "stored sensors data = " + stored_sensors_data_in_string
+    print "new sensors data = " + sensors_data_in_string
+
+    if (stored_sensors_data_in_string is "-1-1-1-1") or (stored_sensors_data_in_string is not sensors_data_in_string):  # if stored data is different than the new data or it's first time, else don't do anything
+        server_reply = ""
+        socket_to_server.sendall(PROTOCOL_UPDATE_KEY + ";" + VEHICLE_TYPE + ";" + VEHICLE_COMPANY + ";" + VEHICLE_NUMBER + ";" + str(len(line_number)) + ";" + line_number + ";" + str(len(sensors_data_in_string)) + ";" + sensors_data_in_string)
+        server_reply = socket_to_server.recv(1024) #1024 - the size of the buffer which recieves the message (int bytes)
+        
+        if server_reply == "r": #server got the data
+            print "Server got the data"
+        elif server_reply == "e": #error - not being used, for future goals
+            #recurrsion - try again to send data to server
+            send_sensors_data_to_server(sensors_data, stored_sensors_data, socket_to_server)
+        
+        SEATS[line_number] = sensors_data_in_string #store the new results
+        
+    socket_to_server.sendall(PROTOCOL_CLOSE_KEY)
+    socket_to_server.close()
+
+
+"""function that will do all the work, get data from the arduino (serial) and update data to server"""
+
+
+def handle_missions():
+    ser = serial.Serial("COM5", SERIAL_PORT) # serial - our "physical socket" to the sensors and the arduino - gets the data of the sensors from the output of the arduino code we made
+    stored_sensors_data = ["-1","-1","-1","-1"]  # for the first run of the functions, then, it changes to the results themselves
+
+    while True:
+        #s;L1;R;L2;A
+        data = ser.readline()
+        if data.startswith('s'):
+            data = data.split(';') #split the data into the relevant parts
+            line_number = str(data[2]) # save in string, helps with the insertions proccess 
+            list_of_sensors = list(data[4])# turn the given string into a list of chars
+            print "".join(list_of_sensors)
+            send_sensors_data_to_server(line_number,list_of_sensors, stored_sensors_data, try_connecting_to_server())  # first cell in the list is always '', so remove it.
+            time.sleep(1)  # sleep for ONE second ( 1s )
+        else:
+            print "error with Arduino's query"
+
+def main():
+    try:
+        handle_missions()
+    except:
+        print "encountered an error"
+
+
+if __name__ == "__main__":
+    main()
---
+++ Server/DataBaseCreator.py
@@ -0,0 +1,146 @@
+import os
+import sqlite3
+
+# Configuration files
+VEHICLE_INSTRUCTOR_FILE = 'Config.txt'
+
+# Path files
+ACCOUNTS_FOLDER = 'Accounts'
+VEHICLES_FOLDER = 'Vehicles'
+
+def prepare_seats_table(c,amount_of_lines):
+	#SEATS - TABLE
+    c.execute('''CREATE TABLE IF NOT EXISTS seats(line int,status text NOT NULL);''')
+    #c.execute('''CREATE TABLE IF NOT EXISTS seats(line int,status text NOT NULL,parts int NOT NULL,chairs_per_part int NOT NULL);''')				
+    
+    result = c.execute('''SELECT line FROM seats WHERE line = ?''',(amount_of_lines,))
+    data = c.fetchall()
+    if  len(data) is 0:
+        x = 1
+        for x in xrange(1, int(amount_of_lines) + 1):
+            try:
+                c.execute('''INSERT INTO seats(line,status) VALUES(?,?);''',(str(x),"0101",))
+            except sqlite3.Error as e:
+                print e
+
+def prepare_active_table(c,start_and_end):
+    #ACTIVE - TABLE
+    c.execute('''CREATE TABLE IF NOT EXISTS active(start_time text NOT NULL,end_time text NOT NULL);''')
+    
+    start_and_end = start_and_end.split("_")
+    for temp in start_and_end:
+        start,end = temp.split(":")
+        result = c.execute('''SELECT start_time FROM active WHERE start_time = ?''',(start,))
+        data = c.fetchall()
+        if len(data) is 0:
+            try:
+                c.execute('''INSERT INTO active(start_time,end_time) VALUES(?,?);''',(str(start),str(end),))
+            except sqlite3.Error as e:
+                print e
+
+def prepare_information_table(c,path_and_delay):
+    #INFORMATION - TABLE
+    c.execute('''CREATE TABLE IF NOT EXISTS information(path text NOT NULL,delay text NOT NULL);''')
+    
+    path_and_delay = path_and_delay.split("_")
+    for temp in path_and_delay:
+        path,delay = temp.split(":")
+        result = c.execute('''SELECT path FROM information WHERE path = ?''',(path,))
+        data = c.fetchall()
+        if len(data) is 0:
+            try:
+                c.execute('''INSERT INTO information(path,delay) VALUES(?,?)'''(str(path),str(delay),))
+            except sqlite3.Error as e:
+                print e
+                
+                
+# checks if the account.db and its foldier exists, otherwise creates it
+def create_account_db():
+    # checks if the accounts folder for the db exist
+    if not os.path.isdir(ACCOUNTS_FOLDER):
+        os.makedirs(ACCOUNTS_FOLDER)
+    else:
+        print "The folder: 'Accounts' already exists!"
+
+    try:
+        print "Connecting to the: 'Accounts' DataBase!"
+        conn = sqlite3.connect(ACCOUNTS_FOLDER + '/Accounts.db')
+        c = conn.cursor()
+
+        print "Creating 'users' table inside the 'Accounts' DataBase!"
+        c.execute('''CREATE TABLE IF NOT EXISTS users(username text NOT NULL,email text NOT NULL,password text NOT NULL,PRIMARY KEY(username,email))''')
+        conn.commit()
+        conn.close()
+
+        print "Accounts DataBase is all set!"
+
+    except sqlite3.Error as e:
+        print e
+        print "Failed to create 'Accounts' DataBase!"
+
+
+def create_vehicle_db():
+    # checks if the configuration file exist
+    print "Checking if the configurationg file exists!"
+    if not os.path.isfile(VEHICLE_INSTRUCTOR_FILE):
+        print "Creating a new basic configurationg file!"
+        temp = open(VEHICLE_INSTRUCTOR_FILE, 'a')
+        # path:delay - RABIN:0000_BIG:0025. START:END - 930:1000_1000:1030
+        temp.write("VEHICLE_TYPE ; VEHICLE_COMPANY ; VEHICLE_NUMBER ; AMOUNT_OF_LINES ; PATH:DELAY_PATH1:DELAY1 ; START:END_START1:END1\n")
+        temp.write("===================================================================================================================\n")
+        temp.close()
+        return
+    else:
+        print "Checking if the Vehicles folder exists!"
+        if not os.path.isdir(VEHICLES_FOLDER):
+            print "Creating a new Vehicles folder!"
+            os.makedirs(VEHICLES_FOLDER)
+
+        print "Reading from the configuration file!"
+        data_file = open(VEHICLE_INSTRUCTOR_FILE)
+        lines = data_file.readlines()
+        data_file.close()
+
+        index = 0  # will be used to skip the format tutorials - the first two lines
+        for line in lines:
+            if index >= 2:
+                # remove whitespaces,\n,lower case, upper case the first letter  and split into the required data
+                vehicle_type, vehicle_company, vehicle_number, amount_of_lines,path_and_delay,start_and_end = line.replace(" ", "").replace('\n',"").lower().title().split(';')
+                dest_path = VEHICLES_FOLDER + '/' + vehicle_type + '/' + vehicle_company + '/' + vehicle_number
+                if not os.path.isdir(VEHICLES_FOLDER + '/' + vehicle_type):
+                    os.makedirs(VEHICLES_FOLDER + '/' + vehicle_type)
+                    os.makedirs(VEHICLES_FOLDER + '/' + vehicle_type + '/' + vehicle_company)
+                    os.makedirs(VEHICLES_FOLDER + '/' + vehicle_type + '/' + vehicle_company + '/' + vehicle_number)
+                else:
+                    if not os.path.isdir(VEHICLES_FOLDER + '/' + vehicle_type + '/' + vehicle_company):
+                        os.makedirs(VEHICLES_FOLDER + '/' + vehicle_type + '/' + vehicle_company)
+                        os.makedirs(VEHICLES_FOLDER + '/' + vehicle_type + '/' + vehicle_company + '/' + vehicle_number)
+                    else:
+                        if not os.path.isdir(VEHICLES_FOLDER + '/' + vehicle_type + '/' + vehicle_company + '/' + vehicle_number):
+                            os.makedirs(VEHICLES_FOLDER + '/' + vehicle_type + '/' + vehicle_company + '/' + vehicle_number)
+
+                conn = sqlite3.connect(dest_path + '/Transport.db')
+                c = conn.cursor()
+                prepare_seats_table(c,amount_of_lines)
+                prepare_active_table(c,start_and_end)
+                prepare_information_table(c,path_and_delay)
+                conn.commit()
+                conn.close()
+            else:    
+                index +=1
+
+
+print "Vehicles DataBase is all set!"
+
+
+def main():
+    try:
+        print "Checking accounts"
+        create_account_db()
+        print "\nChecking vehicles"
+        create_vehicle_db()
+    except:
+        print "ran into global error"
+
+if __name__ == "__main__":
+    main()
